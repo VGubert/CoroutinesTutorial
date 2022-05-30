2eadb14955568beb4573f6077f3d35363abe08b3
Vyacheslav <v.guber2000@gmail.com>
2022-05-30 23:25:48 +0300

    Suspend fun

--- app/src/main/java/com/vgubert/coroutinestutorial/MainActivity.kt
+++ app/src/main/java/com/vgubert/coroutinestutorial/MainActivity.kt
@@ -16,9 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         GlobalScope.launch {
-            delay( 3000L)
-            Log.d(TAG, "Coroutines says hello thread ${Thread.currentThread().name}")
+            val networkCallAnswer = doNetworkCall()
+            val networkCallAnswer2 = doNetworkCall2()
+            Log.d(TAG, networkCallAnswer)
+            Log.d(TAG, networkCallAnswer2)
         }
-        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
+    }
+
+    suspend fun doNetworkCall(): String {
+        delay(3000L)
+        return "This is the answer"
+    }
+
+    suspend fun doNetworkCall2(): String {
+        delay(3000L)
+        return "This is the answer"
     }
 }
