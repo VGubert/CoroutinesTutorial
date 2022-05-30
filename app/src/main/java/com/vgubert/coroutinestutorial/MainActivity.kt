4c6bb63088f7753329fc657ebc620b0b219106c0
Vyacheslav <v.guber2000@gmail.com>
2022-05-30 23:18:00 +0300

    Lesson-1

--- app/src/main/java/com/vgubert/coroutinestutorial/MainActivity.kt
+++ app/src/main/java/com/vgubert/coroutinestutorial/MainActivity.kt
@@ -2,10 +2,23 @@ package com.vgubert.coroutinestutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            delay( 3000L)
+            Log.d(TAG, "Coroutines says hello thread ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 }
